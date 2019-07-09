477a004a5446d93561d38557f5e9b02d8a340c90
quzha <Quanlu.Zhang@microsoft.com>
2019-07-09 14:50:31 +0800

    update

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 from setuptools import setup, find_packages
 from setuptools.extension import Extension
+from setuptools.command.build_ext import build_ext as _build_ext
 import os
 
 
-def get_numpy_include():
-    """Returns Numpy's include path with lazy import.
-    """
-    import numpy as np
-    return np.get_include()
+class build_ext(_build_ext):
+    'to install numpy'
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+        # Prevent numpy from thinking it is still in its setup process:
+        __builtins__.__NUMPY_SETUP__ = False
+        import numpy
+        self.include_dirs.append(numpy.get_include())
 
 # Read http://peterdowns.com/posts/first-time-with-pypi.html to figure out how
 # to publish the package on PyPI
@@ -27,27 +31,27 @@ compiler_directives = {
 extensions = [
     Extension('ConfigSpaceNNI.hyperparameters',
                sources=['ConfigSpaceNNI/hyperparameters.pyx',],
-               include_dirs=[get_numpy_include()],
+               include_dirs=[],
                compiler_directives=compiler_directives),
     Extension('ConfigSpaceNNI.forbidden',
                sources=['ConfigSpaceNNI/forbidden.pyx'],
-               include_dirs=[get_numpy_include()],
+               include_dirs=[],
                compiler_directives=compiler_directives),
     Extension('ConfigSpaceNNI.conditions',
                sources=['ConfigSpaceNNI/conditions.pyx'],
-               include_dirs=[get_numpy_include()],
+               include_dirs=[],
                compiler_directives=compiler_directives),
     Extension('ConfigSpaceNNI.c_util',
                sources=['ConfigSpaceNNI/c_util.pyx'],
-               include_dirs=[get_numpy_include()],
+               include_dirs=[],
                compiler_directives=compiler_directives),
     Extension('ConfigSpaceNNI.util',
                sources=['ConfigSpaceNNI/util.py'],
-               include_dirs=[get_numpy_include()],
+               include_dirs=[],
                compiler_directives=compiler_directives),
     Extension('ConfigSpaceNNI.configuration_space',
                sources=['ConfigSpaceNNI/configuration_space.py'],
-               include_dirs=[get_numpy_include()],
+               include_dirs=[],
                compiler_directives=compiler_directives),
 ]
 
@@ -60,6 +64,7 @@ with open("ConfigSpaceNNI/__version__.py") as fh:
 
 
 setup(
+    cmdclass={'build_txt':build_ext},
     name='ConfigSpaceNNI',
     version=version,
     url='https://github.com/automl/ConfigSpaceNNI',
